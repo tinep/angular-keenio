fbae2832701e62741b91f357ee31587af9027d99
tbk <theborakompanioni@gmail.com>
2015-02-15 02:08:30 +0100

    add more options to tbkKeenConfigProvider

--- src/angular-keenio/services/keen.js
+++ src/angular-keenio/services/keen.js
@@ -7,15 +7,37 @@
     })
 
     .provider('tbkKeenConfig', [function () {
-      var config = {};
+      var config = {
+        projectId: 'YOUR_PROJECT_ID',   // String (required always)
+        writeKey: 'YOUR_WRITE_KEY',     // String (required for sending data)
+        readKey: 'YOUR_READ_KEY',       // String (required for querying data)
+        protocol: 'https',              // String (optional: https | http | auto)
+        host: 'api.keen.io/3.0',        // String (optional)
+        requestType: 'jsonp'            // String (optional: jsonp, xhr, beacon)
+      };
 
       this.projectId = function (projectId) {
         config.projectId = projectId;
         return this;
       };
-
+      this.writeKey = function (writeKey) {
+        config.writeKey = writeKey;
+        return this;
+      };
       this.readKey = function (readKey) {
         config.readKey = readKey;
+        return this;
+      };
+      this.protocol = function (protocol) {
+        config.protocol = protocol;
+        return this;
+      };
+      this.host = function (host) {
+        config.host = host;
+        return this;
+      };
+      this.requestType = function (requestType) {
+        config.requestType = requestType;
         return this;
       };
 
