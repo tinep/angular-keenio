0f8465350a1023556d874d79854912289b0367e1
Pierre Baron <pierre.r.baron22@gmail.com>
2016-02-03 15:29:51 +0000

    Added two new charts

---
+++ src/angular-keenio/directives/keenLinechart.js
@@ -0,0 +1,50 @@
+(function (angular) {
+
+  angular.module('angular-keenio.directives')
+
+    .directive('tbkKeenLinechart', [function() {
+      var d = {
+        scope: {
+          query: '=',
+          title: '@',
+          width: '@',
+          height: '@',
+          isStacked: '@',
+          groupWidth: '@',
+          chartOptions: '=?',
+          colors: '=?',
+          labels: '=?',
+          colorMapping: '=?',
+          labelMapping: '=?'
+        },
+        controller: ['$scope', function($scope) {
+          $scope.chartOptions = $scope.chartOptions || {
+            chartArea: {
+              height: '85%',
+              left: '5%',
+              top: '5%',
+              width: '100%'
+            },
+            bar: {
+              groupWidth: $scope.groupWidth || '85%'
+            },
+            isStacked: !!$scope.isStacked
+          };
+        }],
+        template:
+        '<div data-tbk-keen-chart="linechart" ' +
+        ' query="query" ' +
+        ' height="{{height}}" ' +
+        ' width="{{width}}" ' +
+        ' chart-options="chartOptions" ' +
+        ' colors="colors" ' +
+        ' labels="labels" ' +
+        ' color-mapping="colorMapping" ' +
+        ' label-Mapping="labelMapping" ' +
+        '></div>'
+      };
+
+      return d;
+    }]);
+
+})(angular);
